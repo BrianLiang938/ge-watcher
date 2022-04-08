b53f712e46b09eddfd9fbf981d4d071a7b0ff83a
bgliang <bgliang@ncsu.edu>
2022-04-08 19:15:38 -0400

    added commands

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const { Client, Intents } = require('discord.js');
-const { token } = require("process.env");
+const fs = require('node:fs');
+const { Client, Intents, Collection } = require('discord.js');
+const { token } = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
 client.login(token);
-client.on('ready', readyDiscord);
+console.log('Running');
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
-//const api_url = 'https://secure.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=556';
+	const command = client.commands.get(interaction.commandName);
 
-/*async function test() {
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	}
+    catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+// const api_url = 'https://secure.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=556';
+
+/* async function test() {
      const request = await fetch(api_url);
      const json = await request.json();
      const item = json.item;
@@ -23,9 +46,5 @@ function bot() {
     client.on('ready', readyDiscord);
 }
 */
-
-function readyDiscord() {
-    console.log("Im in B)");
-}
-//bot();
-//test();
+// bot();
+// test();
