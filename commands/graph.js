de5486863b305be5d222f6fb744e437a838755a1
bgliang <bgliang@ncsu.edu>
2022-04-11 22:01:48 -0400

    commit

---
+++ commands/graph.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fetch = require('node-fetch');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('graph')
+		.setDescription('Displays graph for item')
+        .addStringOption(option => option.setName('input').setDescription('Enter a string')),
+	async execute(interaction) {
+		const input = interaction.options.getString('input');
+		const api_url = `https://secure.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=${input}`;
+		const request = await fetch(api_url);
+		const json = await request.json();
+		const item = json.item;
+		const { name } = item;
+		const price = item.current.price;
+		await interaction.reply('/search ' + input + '\nName of the item: ' + name + '\nPrice of the item: ' + price + ' gp');
+	},
+};
