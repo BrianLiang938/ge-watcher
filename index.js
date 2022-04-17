0c84cd6fd7ef044eb4a010ec791e85c8ec486627
bgliang <bgliang@ncsu.edu>
2022-04-16 23:06:19 -0400

    fixed promise issue

--- index.js
+++ index.js
@@ -27,24 +27,3 @@ client.on('interactionCreate', async interaction => {
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
-
-// const api_url = 'https://secure.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=556';
-
-/* async function test() {
-     const request = await fetch(api_url);
-     const json = await request.json();
-     const item = json.item;
-     console.log(json);
-     const {members, id} = item;
-     console.log(id);
-     console.log(members);
-}
-
-function bot() {
-    const client = new Discord.Client();
-    client.login('OTYxNDExNzkwNzE2MTc0Mzg4.Yk4mgQ.M1nAgjcBoIKNjWAu0qc3WN_FTyA');
-    client.on('ready', readyDiscord);
-}
-*/
-// bot();
-// test();
