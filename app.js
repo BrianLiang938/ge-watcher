8abd9731eefc9b5387ccf38f9d03733f1616a5d9
bgliang <bgliang@ncsu.edu>
2022-04-18 23:54:24 -0400

    added average over time, seperated disc and web

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const fetch = require('node-fetch');
 const express = require('express');
 const app = express();
 const port = 3000;
+// number of times to take an average value
+const back = 20;
+// number of values to pull the average from
+const movingavg = 20;
 
 app.use('/:id', async function(req, res, next) {
     const id = req.params.id;
@@ -9,26 +13,46 @@ app.use('/:id', async function(req, res, next) {
     const api_url = `https://prices.runescape.wiki/api/v1/osrs/timeseries?timestep=5m&id=${id}`;
     const request = await fetch(api_url);
     const json = await request.json();
-    const size = Object.keys(json.data).length;
     const data = json.data;
+    req.data = data;
+    next();
+});
+app.get('/:id', function(req, res) {
     const vals = [];
-    let avg = 0;
+    const averages = [];
+    for (let i = 0; i < movingavg; i++) {
+        vals.push(req.data.at(-(i + 1)));
+    }
+    let avgVal = average(vals);
+    averages.unshift(avgVal);
+    console.log('first ' + averages.at(0));
+    for (let i = 0; i < back; i++) {
+        vals.shift();
+        vals.push(req.data.at(-(i + 21)));
+        avgVal = average(vals);
+        averages.push(avgVal);
+    }
+    let string = '';
+    for (let i = 0; i < back + 1; i++) {
+        string = string + 'Average value at time ' + i + ' is ' + averages.at(i);
+        string = string + '<br/>';
+    }
+    res.send(string);
+});
+
+function average(array) {
+    let output = 0;
     let count = 0;
     for (let i = 0; i < 20; i++) {
-        vals.push(data[size - 1 - i]);
-        const high = data[size - 1 - i].avgHighPrice;
+        const high = array.at(i).avgHighPrice;
         if (high != null) {
             count++;
         }
         const tax = Math.floor(high * 0.01);
-        avg += high - tax;
-        req.average = avg / count;
+        output += high - tax;
     }
-    next();
-});
-app.get('/:id', function(req, res) {
-    res.send('Average is ' + req.average);
-});
+    return output / count;
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
 app.use(express.static('public'));
--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-const fs = require('node:fs');
-const { Client, Intents, Collection } = require('discord.js');
-const { token } = require('./config.json');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const express = require('express');
+const path = require('path');
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
-}
-client.login(token);
-console.log('Running');
+const app = express();
+const port = process.env.PORT || 8080;
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	}
-    catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
