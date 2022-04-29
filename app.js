efa8923b29ae7b5f9183afcebebb7a96f665ee61
bgliang <bgliang@ncsu.edu>
2022-04-29 16:46:06 -0400

    shifted to add array to points db

--- app.js
+++ app.js
@@ -1,62 +1,12 @@
-const fetch = require('node-fetch');
-const express = require('express');
-const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
 // number of times to take an average value
-const back = 20;
 // number of values to pull the average from
-const movingavg = 20;
+const { mongoDB } = require('./config.json');
 
-const favicon = require('serve-favicon');
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use(favicon(__dirname + '/favicon.ico'));
-
-async function dataPull(req, res, next) {
-    const id = req.params.id;
-    console.log(id);
-    const api_url = `https://prices.runescape.wiki/api/v1/osrs/timeseries?timestep=5m&id=${id}`;
-    const request = await fetch(api_url);
-    const json = await request.json();
-    const data = json.data;
-    req.data = data;
-    next();
-}
-app.get('/:id', dataPull, function(req, res) {
-    const vals = [];
-    const averages = [];
-    for (let i = 0; i < movingavg; i++) {
-        vals.push(req.data.at(-(i + 1)));
-    }
-    let avgVal = average(vals);
-    averages.unshift(avgVal);
-    console.log('first ' + averages.at(0));
-    for (let i = 0; i < back; i++) {
-        vals.shift();
-        vals.push(req.data.at(-(i + 21)));
-        avgVal = average(vals);
-        averages.push(avgVal);
-    }
-    let string = '';
-    for (let i = 0; i < back + 1; i++) {
-        string = string + 'Average value at time ' + i + ' is ' + averages.at(i);
-        string = string + '<br/>';
-    }
-    res.send(string);
-});
-
-function average(array) {
-    let output = 0;
-    let count = 0;
-    for (let i = 0; i < 20; i++) {
-        const high = array.at(i).avgHighPrice;
-        if (high != null) {
-            count++;
-        }
-        const tax = Math.floor(high * 0.01);
-        output += high - tax;
-    }
-    return output / count;
-}
-
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+db.collection('points').deleteMany({});
+console.log('Deleted values');
+process.exit(1);
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Points = require('./point.js');
 const fetch = require('node-fetch');
 
 // number of times to take an average value
-const back = 20;
+const back = 20 - 1;
 // number of values to pull the average from
 const movingavg = 20;
 
@@ -25,83 +25,85 @@ app.use(favicon(__dirname + '/favicon.ico'));
 
 
 async function dataPull(req, res, next) {
-  const id = req.params.id;
-  console.log(id);
-  const api_url = `https://prices.runescape.wiki/api/v1/osrs/timeseries?timestep=5m&id=${id}`;
-  const request = await fetch(api_url);
-  const json = await request.json();
-  const data = json.data;
-  req.data = data;
-  next();
+    const id = req.params.id;
+    console.log(id);
+    const api_url = `https://prices.runescape.wiki/api/v1/osrs/timeseries?timestep=5m&id=${id}`;
+    const request = await fetch(api_url);
+    const json = await request.json();
+    const data = json.data;
+    req.data = data;
+    next();
 }
 
 app.get('/:id', dataPull, function(req, res) {
-  const vals = [];
-  const averages = [];
-  const devs = [];
-  for (let i = 0; i < movingavg; i++) {
-    // newest datapoint at the back
-    vals.push(req.data.at(-(i + 1)));
-  }
-  let avgVal = average(vals);
-  averages.unshift(avgVal);
-  for (let i = 0; i < back; i++) {
-    vals.shift();
-    vals.push(req.data.at(-(i + 21)));
-    avgVal = average(vals);
-    averages.push(avgVal);
-    const currDev = standardDev(vals, avgVal);
-    devs.push(currDev);
-  }
-  let string = '';
-  for (let i = 0; i < back + 1; i++) {
-    string = string + 'Average value at time ' + i + ' is ' + averages.at(i);
-    string = string + '<br/>';
-  }
-  //
-  const currPoint = makePoint(req.params.id, req.data.at(-1).timestamp, req.data.at(-1).avgHighPrice, averages[0], devs[0]);
-  console.log(currPoint);
-  db.collection('points').insertOne(currPoint);
-  res.send(string);
+    const vals = [];
+    const averages = [];
+    const devs = [];
+    for (let i = 0; i < movingavg; i++) {
+        // newest datapoint at the back
+        vals.push(req.data.at(-(i + 1)));
+    }
+    let avgVal = average(vals);
+    averages.unshift(avgVal);
+    for (let i = 0; i < back; i++) {
+        vals.shift();
+        vals.push(req.data.at(-(i + 21)));
+        avgVal = average(vals);
+        averages.push(avgVal);
+        const currDev = standardDev(vals, avgVal);
+        devs.push(currDev);
+    }
+    let string = '';
+    const points = [];
+    for (let i = 0; i < back + 1; i++) {
+        const currPoint = makePoint(req.params.id, req.data.at(-(i + 1)).timestamp, req.data.at(-(i + 1)).avgHighPrice, averages[i], devs[i]);
+        points[i] = currPoint;
+        console.log(currPoint);
+        string = string + 'Average value at time ' + (i + 1) + ' is ' + averages.at(i);
+        string = string + '<br/>';
+    }
+    db.collection('points').insertMany(points);
+    //
+    res.send(string);
 });
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
 
 function average(array) {
-  let output = 0;
-  let count = 0;
-  for (let i = 0; i < 20; i++) {
-    const high = array.at(i).avgHighPrice;
-    if (high != null) {
-      count++;
+    let output = 0;
+    let count = 0;
+    for (let i = 0; i < 20; i++) {
+        const high = array.at(i).avgHighPrice;
+        if (high != null) {
+            count++;
+        }
+        const tax = Math.floor(high * 0.01);
+        output += high - tax;
     }
-    const tax = Math.floor(high * 0.01);
-    output += high - tax;
-  }
-  return output / count;
+    return output / count;
 }
 
 function standardDev(array, avg) {
-  let diff = 0;
-  let output = 0;
-  let count = 0;
-  for (let i = 0; i < 20; i++) {
-    const high = array.at(i).avgHighPrice;
-    const tax = Math.floor(high * 0.01);
-    diff = (high - tax) - avg;
-    output += diff * diff;
-    if (high != null) {
-      count++;
+    let diff = 0;
+    let output = 0;
+    let count = 0;
+    for (let i = 0; i < 20; i++) {
+        const high = array.at(i).avgHighPrice;
+        const tax = Math.floor(high * 0.01);
+        diff = (high - tax) - avg;
+        output += diff * diff;
+        if (high != null) {
+            count++;
+        }
     }
-  }
-  return Math.sqrt(output / count);
+    return Math.sqrt(output / count);
 }
 
 function makePoint(id, pDate, pPrice, pMean, pStddev) {
-  const output = new Points({ id: id, date: pDate, price:pPrice, mean:pMean, stdDev: pStddev });
-  if (pMean - pStddev > pPrice) {
-    output.buy = true;
-  }
-  return output;
+    const output = new Points({ id: id, date: pDate, price: pPrice, mean: pMean, stdDev: pStddev });
+    if (pMean - pStddev > pPrice) {
+        output.buy = true;
+    }
+    return output;
 }
